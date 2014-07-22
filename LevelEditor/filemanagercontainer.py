f3b55caefb67247bb839cd43f416f4538b40168d
brandr <magicalfarmrancherseries@yahoo.com>
2014-07-22 11:50:49 -0700

    dungeon saving and loading has been improved.

--- LevelEditor/filemanagercontainer.py
+++ LevelEditor/filemanagercontainer.py
@@ -28,63 +28,68 @@ class FileManagerContainer(Box):
 		self.topleft = (position[0], position[1])
 		
 		#init data specific to this container
-		self.level_select_container, self.dungeon_grid_container = level_select_container,dungeon_grid_container
-		self.selected_slot = None
+		self.level_select_container, self.dungeon_grid_container = level_select_container, dungeon_grid_container
+		#self.current_file = None
 
 		#create components
-		self.new_dungeon_button = self.new_dungeon_button(8,8)
-		self.save_dungeon_button = self.save_dungeon_button(self.new_dungeon_button.right+16,self.new_dungeon_button.top)
-		self.load_dungeon_button = self.load_dungeon_button(self.save_dungeon_button.right+16,self.save_dungeon_button.top)
-		self.selected_slot_label = self.selected_slot_label(dimensions[0]-164,8)
-		self.file_slot_window = self.file_slot_window(self.selected_slot_label.left+32,self.selected_slot_label.bottom+8)
+		self.new_dungeon_button = self.new_dungeon_button(8, 8)
+		self.save_dungeon_button = self.save_dungeon_button(self.new_dungeon_button.right + 16, self.new_dungeon_button.top)
+		self.load_dungeon_button = self.load_dungeon_button(self.save_dungeon_button.right + 16, self.save_dungeon_button.top)
+		self.current_file_label = self.current_file_label(dimensions[0] - 320, 8)
+		self.filename_entry = self.filename_entry(self.current_file_label.right + 8, self.current_file_label.top - 4)
+		self.file_select_window = self.file_select_window(self.current_file_label.left, self.current_file_label.bottom + 8)
+		
 		#TODO: delete dungeon data button? Other buttons?
 
 		#add components to contaier
 		self.add_child(self.new_dungeon_button)
 		self.add_child(self.save_dungeon_button)
 		self.add_child(self.load_dungeon_button)
-		self.add_child(self.file_slot_window)
-		self.add_child(self.selected_slot_label)
-
+		self.add_child(self.file_select_window)
+		self.add_child(self.filename_entry)
+		self.add_child(self.current_file_label)
+		
 		self.updateFileSelection()
 
 	#methods for making GUI components
 
-	def new_dungeon_button(self,x,y):
+	def new_dungeon_button(self, x, y):
 		button = Button("New Dungeon")
-		button.topleft = x,y
-		button.connect_signal(SIG_CLICKED,self.level_select_container.editor_screen.resetEditor) 
+		button.topleft = x, y
+		button.connect_signal(SIG_CLICKED, self.level_select_container.editor_screen.resetEditor) 
 		return button
 
-	def save_dungeon_button(self,x,y):
+	def save_dungeon_button(self, x, y):
 		button = Button("Save Dungeon")
-		button.topleft = x,y
-		button.connect_signal(SIG_CLICKED,self.saveDungeon)
+		button.topleft = x, y
+		button.connect_signal(SIG_CLICKED, self.saveDungeon)
 		return button
 
-	def load_dungeon_button(self,x,y):
+	def load_dungeon_button(self, x, y):
 		button = Button("Load Dungeon")
-		button.topleft = x,y
-		button.connect_signal(SIG_CLICKED,self.loadDungeon)
+		button.topleft = x, y
+		button.connect_signal(SIG_CLICKED, self.loadDungeon)
 		return button
 
-	def file_slot_window(self,x,y): 
-		window = ScrolledList(72,72)
-		window.topleft = x,y
-		file_slot_collection = FileManagerContainer.default_file_slots()
-		window.set_items(file_slot_collection)
-		window.set_selectionmode(SELECTION_SINGLE)
-		window.connect_signal(SIG_SELECTCHANGED,self.changeSelection,window)
-		return window
+	def file_select_window(self, x, y): 
+		file_list = FileList (280, 72, "./dungeon_map_files")
+		file_list.topleft = x, y
+		file_list.connect_signal(SIG_SELECTCHANGED, self.change_selection, file_list)
+		return file_list
 
-	def selected_slot_label(self,x,y):
-		label = Label ("Selected slot:     ")
-		label.topleft = x,y
+	def current_file_label(self, x, y):
+		label = Label ("Selected save: ")
+		label.topleft = x, y
 		return label
+
+	def filename_entry(self, x, y):
+		entry = Entry("                  ")
+		entry.topleft = x, y
+		return entry
 
 	#methods that affect components
 
-	def setSensitivity(self,component,sensitive):
+	def setSensitivity(self, component, sensitive):
 		state = Constants.STATE_INSENSITIVE
 		if(sensitive): state = Constants.STATE_NORMAL
 		component.set_state(state)
@@ -93,25 +98,25 @@ class FileManagerContainer(Box):
 	#save-related methods
 
 	def saveDungeon(self):
-		if self.selected_slot == None: return
+		current_filename = self.current_filename()
+		if current_filename == None: return
 		print "Saving dungeon..."
-		slot = self.selected_slot
-		filename = "./dungeon_map_files/dungeon" + slot
-		dungeon_file = open(filename,'wb') #'wb' means "write binary"
+		filename = "./dungeon_map_files/" + current_filename
+		dungeon_file = open(filename, 'wb') #'wb' means "write binary"
 		dungeon_data = self.dungeon_save_data()
 		save_data = dungeon_data.formatted_data()
 		json.dump(save_data, dungeon_file)
 		
 		dev_filepath = "C:\Users\Robert\Documents\python_stuff\Platformer\dungeon_map_files"
 		if(path.exists(dev_filepath)):
-			dev_filename = dev_filepath + "\dungeon" + slot
+			dev_filename = dev_filepath + "\\" + current_filename
 			print "Saving dungeon for the developers..."
 			dev_dungeon_file = open(dev_filename,'wb') #'wb' means "write binary"
 			json.dump(save_data, dev_dungeon_file)
 		else:
 			print "ERROR: failed to save for developers."
 
-		print "Dungeon saved."
+		print "Dungeon saved. \n"
 
 	def dungeon_save_data(self): #return a DungeonData object used for reading/writing files
 		#TODO: (make sure to deal with ununsed rooms/levels properly when building the DungeonData)
@@ -123,20 +128,20 @@ class FileManagerContainer(Box):
 	#load method
 
 	def loadDungeon(self):
-		if self.selected_slot == None: return
-		slot = self.selected_slot
-		filename = "./dungeon_map_files/dungeon" + slot
+		current_filename = self.current_filename()
+		if current_filename == None: return
+		filename = "./dungeon_map_files/" + current_filename
 		deformatted_dungeon = FileManagerContainer.dungeonDataFromFile(filename)
 		self.buildDungeon(deformatted_dungeon)
 
 	@staticmethod
 	def dungeonDataFromFile(filename, filepath = "./"):
-		dungeon_file = open(filename,'rb') #'rb' means "read binary"
+		dungeon_file = open(filename, 'rb') #'rb' means "read binary"
 		dungeon_data = json.load(dungeon_file) #this part reads the data from file
-		deformatted_dungeon = DungeonData.deformatted_dungeon(dungeon_data,filepath)
+		deformatted_dungeon = DungeonData.deformatted_dungeon(dungeon_data, filepath)
 		return deformatted_dungeon
 
-	def buildDungeon(self,dungeon_data): #uncomment the prints in this method to test load times.
+	def buildDungeon(self, dungeon_data): #uncomment the prints in this method to test load times.
 		if dungeon_data == None: return
 		print "Building dungeon..."
 		level_data_set = dungeon_data.level_data_set
@@ -146,34 +151,21 @@ class FileManagerContainer(Box):
 		self.dungeon_grid_container.setRooms(room_data_set) 
 		print "Loading levels..."
 		self.level_select_container.setLevels(level_data_set) 
-		print "Dungeon built."
+		print "Dungeon built. \n"
 
 	#methods for file slot selection
 
-	def changeSelection(self,window):
-		slot = self.file_slot_window.get_selected()
-		self.selected_slot = None
-		if slot != None and len(slot) >= 1:
-			self.selected_slot = slot[0].text
+	def current_filename(self):
+		return self.filename_entry._text
+
+	def change_selection(self, window):
+		selected_file = self.file_select_window.get_selected()
+		filename = selected_file[0]._text
+		self.filename_entry.set_text(filename)	# error checking?
 		self.updateFileSelection()
 
 	def updateFileSelection(self):
-		selection_string = "Selected slot: "
-		slot = self.selected_slot
-		if slot == None:
-			selection_string += "None"
-		else:
-			selection_string += str(slot)
-		self.selected_slot_label.set_text(selection_string)
-		self.setSensitivity(self.save_dungeon_button,self.selected_slot != None)
-		self.setSensitivity(self.load_dungeon_button,self.selected_slot != None)
-
-	#static methods
-
-	@staticmethod
-	def default_file_slots():
-		slot_count = DEFAULT_FILE_COUNT
-		collection = ListItemCollection()
-		for n in xrange(slot_count):
-			collection.append(TextListItem(str(n)))
-		return collection
+		current_filename = self.current_filename()
+		valid_filename = current_filename != None and len(current_filename) > 0	and current_filename != ".." #TODO: consider checking for spaces and stuff too
+		self.setSensitivity(self.save_dungeon_button, valid_filename)
+		self.setSensitivity(self.load_dungeon_button, valid_filename)
